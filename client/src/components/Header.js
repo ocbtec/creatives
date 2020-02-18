048ed8136a8794e82cf75cbb5b530c420675637a
ocbtec <tepfenhart@gmx.de>
2020-02-18 10:27:57 +0100

    fixed navbar layout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,6 +6,26 @@ import Menu from './Menu';
 import '../css/header.css';
 
 const Header = ({ userName, avatarImage, token }) => {
+  let menuDisplayed = false;
+  const menuFadeInOut = () => {
+    let menu = document.getElementsByClassName('menu')[0];
+    let menubars = document.querySelectorAll('.menubars');
+
+    if (menuDisplayed) {
+      menu.className = 'menu menu-fade-out';
+      menubars.forEach((el, index) => {
+        el.className = `menubars menubar-${index + 1} menubar-${index +
+          1}-close-menu`;
+      });
+    } else {
+      menu.className = 'menu menu-fade-in';
+      menubars.forEach((el, index) => {
+        el.className = `menubars menubar-${index + 1} menubar-${index +
+          1}-open-menu`;
+      });
+    }
+    menuDisplayed = !menuDisplayed;
+  };
   return (
     <Fragment>
       <div className='header'>
@@ -33,6 +53,13 @@ const Header = ({ userName, avatarImage, token }) => {
             token={token}
           />
         )}
+        <div className='burger-menu-container'>
+          <button className='menu-button' onClick={menuFadeInOut}>
+            <div className='menubars menubar-1'></div>
+            <div className='menubars menubar-2'></div>
+            <div className='menubars menubar-3'></div>
+          </button>
+        </div>
       </div>
       <Menu userName={userName} avatarImage={avatarImage} token={token} />
     </Fragment>
