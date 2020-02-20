5c62c36397561609a534c9bd8c6f31b98166f982
ocbtec <tepfenhart@gmx.de>
2020-02-20 10:19:57 +0100

    solved merge conflicts

--- client/src/components/CategoryButton.js
+++ client/src/components/CategoryButton.js
@@ -1,20 +1,6 @@
 import React, { Fragment } from 'react';
 
 const CategoryButton = props => {
-<<<<<<< HEAD
-  let iconPathSelect = [];
-  let iconPathDeselect = [];
-
-  props.categories.map(category => {
-    iconPathSelect.push(`../images/${category.categoryIconPath}-select.png`);
-    iconPathDeselect.push(
-      `../images/${category.categoryIconPath}-deselect.png`
-    );
-    return [iconPathSelect, iconPathDeselect];
-  });
-
-=======
->>>>>>> 326f5e94ca994eeb1a156c67dc41f5f284f362b9
   return (
     <Fragment>
       <button
