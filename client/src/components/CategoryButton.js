6a53cbc5b8a98ee73b478fe51d60e6827c33b226
jsk900 <jsk900@yahoo.co.uk>
2020-02-20 04:05:28 +0100

    Fixed category icons on register creatives page

--- client/src/components/CategoryButton.js
+++ client/src/components/CategoryButton.js
@@ -1,26 +1,6 @@
 import React, { Fragment } from 'react';
 
 const CategoryButton = props => {
-  let iconPathSelect = [];
-  let iconPathDeselect = [];
-
-  props.categories.map(category => {
-    iconPathSelect.push(`../images/${category.categoryIconPath}-select.png`);
-    iconPathDeselect.push(
-      `../images/${category.categoryIconPath}-deselect.png`
-    );
-    return [iconPathSelect, iconPathDeselect];
-  });
-  console.log(props.index);
-
-  let imageIcon;
-  props.categoryIcon === 'select'
-    ? (imageIcon = iconPathSelect[props.index])
-    : (imageIcon = iconPathDeselect[props.index]);
-
-  let bla = require([imageIcon]);
-  // console.log(bla);
-
   return (
     <Fragment>
       <button
@@ -29,7 +9,11 @@ const CategoryButton = props => {
         onClick={e => {
           props.handleCategoryClick(e);
         }}>
-        {/* <img className='category-icon' src={bla} /> */}
+        <img
+          className='category-icon'
+          src={props.categoryIcon}
+          alt={props.categoryName}
+        />
         <p className={props.categoryName + ' category-label'}>
           {props.categoryName}
         </p>
