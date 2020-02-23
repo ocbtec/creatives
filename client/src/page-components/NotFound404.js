307a93af0c1c1f81d7bdb8977dd8a6e33463cc13
ocbtec <tepfenhart@gmx.de>
2020-02-23 13:31:53 +0100

    function renaming

--- client/src/page-components/NotFound404.js
+++ client/src/page-components/NotFound404.js
@@ -1,11 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
 import '../css/notFound404.css';
 
 const NotFound404 = props => {
-  console.log(props.history);
-  const goBack = e => {
+  const backClick = e => {
     e.preventDefault();
     props.history.go(-1);
   };
@@ -15,7 +15,7 @@ const NotFound404 = props => {
       <div className='main-container'>
         <Header2 />
 
-        <button id='back-button' onClick={goBack}>
+        <button id='back-button' onClick={backClick}>
           Back
         </button>
 
