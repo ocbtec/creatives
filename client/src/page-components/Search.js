8f26485b5b140dfc8613acd7e3723bd135417a15
ocbtec <tepfenhart@gmx.de>
2020-02-27 19:47:59 +0100

    styled page

--- client/src/page-components/Search.js
+++ client/src/page-components/Search.js
@@ -10,7 +10,7 @@ import '../css/search.css';
 const Search = props => {
   //States
   const [formData, setFormData] = useState({
-    type: 'creations',
+    type: 'creatives',
     city: '',
     text: '',
     category: '',
@@ -45,21 +45,22 @@ const Search = props => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  let creationsSelected = 'select';
-  let creativesSelected = 'deselect';
+  let creativesSelected = 'select';
+  let creationsSelected = 'deselect';
 
   const typeToggleClick = e => {
     e.preventDefault();
     setFormData({ ...formData, type: e.currentTarget.name });
-
-    console.log(e.target);
-    console.log(e.currentTarget.name);
 
     if (e.target.name === 'creatives') {
       e.target.src = `/images/${e.target.name}-icon-select.png`;
       document.querySelector(
         `.search-creations-toggle-image`
       ).src = `/images/creations-icon-deselect.png`;
+      document.querySelector(`.creativ-icon-container`).style =
+        'border-radius: 50%; background-color: #0ad82e; box-shadow: 0px 0px 30px 20px #0ad82e;';
+      document.querySelector(`.creation-icon-container`).style =
+        'border-radius: 50%; background-color: #ffffff00; box-shadow: 0px 0px 0px 0px #ffffff00;';
       setClickFlag(true);
     }
     if (e.target.name === 'creations') {
@@ -67,6 +68,10 @@ const Search = props => {
       document.querySelector(
         `.search-creatives-toggle-image`
       ).src = `/images/creatives-icon-deselect.png`;
+      document.querySelector(`.creation-icon-container`).style =
+        'border-radius: 50%; background-color: #0ad82e; box-shadow: 0px 0px 30px 20px #0ad82e;';
+      document.querySelector(`.creativ-icon-container`).style =
+        'border-radius: 50%; background-color: #ffffff00; box-shadow: 0px 0px 0px 0px #ffffff00;';
       setClickFlag(false);
     }
   };
@@ -78,6 +83,60 @@ const Search = props => {
   const radioCategoryClick = (e, index) => {
     setFormData({ ...formData, category: e.target.name });
     setChecked(index);
+
+    let categoryButtons = document.querySelectorAll('.search-category-button');
+
+    let categoryCache = e.target.name;
+
+    categoryButtons.forEach(category => {
+      const setSelectedCategory = () => {
+        category.className =
+          'search-category-button search-category-button-deselect';
+        category.firstChild.src = `/images/category-icon-${category.firstChild.id
+          .toLowerCase()
+          .replace(' ', '-')}-deselect.png`;
+        category.lastChild.style = 'color: #616869';
+      };
+      const setDeselectedCategories = () => {
+        e.target.className =
+          'search-category-button search-category-button-select';
+        e.target.firstChild.src = `/images/category-icon-${e.target.firstChild.id
+          .toLowerCase()
+          .replace(' ', '-')}-select.png`;
+        e.target.lastChild.style = 'color: #fefefe';
+      };
+      // console.log(categoryCache);
+      // console.log(category.name);
+
+      if (categoryCache === category.name) {
+        setDeselectedCategories();
+        return;
+      } else {
+        setDeselectedCategories();
+        setSelectedCategory();
+      }
+    });
+  };
+
+  const tagSelect = e => {
+    let categoryCheck = formData.tags.indexOf(e.target);
+    console.log(categoryCheck);
+    // console.log(formData.tags);
+    console.log(e.target);
+    console.log(e.target.id);
+    // console.log(categoryTags);
+
+    if (categoryCheck > -1) {
+      // deselected
+      formData.tags.splice(categoryCheck, 1);
+      e.target.className = 'tag-deselect';
+    } else {
+      // selected
+      formData.tags.push(e.target.id);
+      e.target.className = 'tag-select';
+
+      console.log(formData.tags);
+    }
   };
 
   //Redirect handler
@@ -167,24 +226,28 @@ const Search = props => {
           </div>
           <div className='search-container'>
             <form
-              className='search-container-flex-1 search-form-container'
+              className='search-container-flex-1'
               onSubmit={e => onSubmit(e)}>
               <div className='search-select-label'>Select</div>
               <div className='search-select-main-buttons'>
-                <img
-                  className='search-creatives-toggle-image'
-                  src={`/images/creatives-icon-${creativesSelected}.png`}
-                  alt='Creatives icon'
-                  name='creatives'
-                  onClick={e => typeToggleClick(e)}
-                />
-                <img
-                  className='search-creations-toggle-image'
-                  src={`/images/creations-icon-${creationsSelected}.png`}
-                  alt='Creations icon'
-                  name='creations'
-                  onClick={e => typeToggleClick(e)}
-                />
+                <div className='creativ-icon-container'>
+                  <img
+                    className='search-creatives-toggle-image'
+                    src={`/images/creatives-icon-${creativesSelected}.png`}
+                    alt='Creatives icon'
+                    name='creatives'
+                    onClick={e => typeToggleClick(e)}
+                  />
+                </div>
+                <div className='creation-icon-container'>
+                  <img
+                    className='search-creations-toggle-image'
+                    src={`/images/creations-icon-${creationsSelected}.png`}
+                    alt='Creations icon'
+                    name='creations'
+                    onClick={e => typeToggleClick(e)}
+                  />
+                </div>
               </div>
 
               <input
@@ -209,29 +272,46 @@ const Search = props => {
               <button className='search-button'>Search</button>
             </form>
             <div className='search-container-flex-2'>
-              <h2>Categories</h2>
-              <div className='search-category-buttons'>
+              <h2 className='search-h2'>Category</h2>
+              <div className='search-optional-label'>optional</div>
+              <div className='search-category-container'>
                 {categoriesProps.map((category, index) => {
                   return (
-                    <label key={index} htmlFor={category.categoryName}>
-                      <input
-                        key={index}
-                        type='radio'
-                        checked={checked === index ? true : false}
+                    <button
+                      key={index}
+                      htmlFor={category.categoryName}
+                      className='search-category-button search-category-button-deselect'
+                      name={category.categoryName}
+                      onClick={e => radioCategoryClick(e, index)}>
+                      <img
+                        className='category-icon'
+                        src={`/images/${category.categoryIconPath}-deselect.png`}
+                        alt={category.categoryName}
                         id={category.categoryName}
-                        name={category.categoryName}
-                        value={category.categoryName}
-                        onChange={e => radioCategoryClick(e, index)}
                       />
-                      {category.categoryName}
-                    </label>
+                      <p className={category.categoryName + ' category-label'}>
+                        {category.categoryName}
+                      </p>
+                    </button>
                   );
                 })}
-                <p>{categoryTags}</p>
               </div>
             </div>
             <div className='search-container-flex-3'>
-              <h2>Tags</h2>
+              <h2 className='search-h2'>Tags</h2>
+              <div className='search-optional-label'>optional</div>
+              <div className='search-tags-container'>
+                {categoryTags.map(tag => {
+                  return (
+                    <div
+                      className='tag-deselect'
+                      id={tag}
+                      onClick={e => tagSelect(e)}>
+                      {tag}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
