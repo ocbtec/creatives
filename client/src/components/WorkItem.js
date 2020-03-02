270340ad19eca9fa1a607bd02771ae7a7cd44e8a
ocbtec <tepfenhart@gmx.de>
2020-03-02 19:48:56 +0100

    styled page

--- client/src/components/WorkItem.js
+++ client/src/components/WorkItem.js
@@ -18,33 +18,30 @@ const WorkItem = props => {
 
   return (
     <div className='work-item'>
-      <div className='work-item-flex-container'>
-        <div className='work-item-title-left'>Title</div>
-        <Link
-          className='work-item-link'
-          to={{
-            pathname: '/workDetail',
-            state: {
-              userName: props.userName,
-              avatarImage: props.avatarImage,
-              token: props.token,
-              creative: props.creative,
-              creativeName: props.creativeName,
-              creativeAvatar: props.creativeAvatar,
-              categories: props.categories,
-              workId: props.workId
-            }
-          }}>
-          <div className='work-item-flex-right'>
-            <div
-              className='work-item-title-right-icon'
-              style={{
-                backgroundImage: 'url(./images/works-icon-dark.png)'
-              }}></div>
-            <div>{props.fileTitle}</div>
-          </div>
-        </Link>
-      </div>
+      <Link
+        className='work-item-link'
+        to={{
+          pathname: '/creativeDetail',
+          state: {
+            userName: props.userName,
+            avatarImage: props.avatarImage,
+            token: props.token,
+            creative: props.creative,
+            categories: props.categories,
+            creativeName: props.creativeName
+          }
+        }}>
+        <div className='work-item-creative-name'>
+          <img
+            className='showcase-avatar-icon'
+            src={`${props.creativeAvatar}`}
+            alt='Avatar'
+          />
+
+          <div className='work-item-flex-item-right'>{props.creativeName}</div>
+        </div>
+      </Link>
+
       <div className='work-image-wrapper'>
         <Link
           to={{
@@ -62,53 +59,40 @@ const WorkItem = props => {
           }}>
           <div
             className='work-image-container'
-            style={{ backgroundImage: `url(${props.filePath})` }}></div>
-        </Link>
-      </div>
-
-      <div className='work-item-flex-container'>
-        <div className='work-item-category-left'>Category</div>
-
-        <div className='work-item-flex-right'>
-          <div className='showcase-category-icon-container'>
-            <img
-              className='showcase-category-icon'
-              src={`./images/${categoryIcon}-deselect.png`}
-              alt='Category icon'
-            />
-          </div>
-          <div>{props.fileCategory}</div>
-        </div>
-      </div>
-      <div id='flex-container-creator' className='work-item-flex-container'>
-        <div className='work-item-creator-left'>Creator</div>
-
-        <Link
-          className='work-item-link'
-          to={{
-            pathname: '/creativeDetail',
-            state: {
-              userName: props.userName,
-              avatarImage: props.avatarImage,
-              token: props.token,
-              creative: props.creative,
-              categories: props.categories,
-              creativeName: props.creativeName
-            }
-          }}>
-          <div className='work-item-flex-right'>
-            <img
-              className='showcase-avatar-image'
-              src={`${props.creativeAvatar}`}
-              alt='Avatar'
-            />
-
-            <div className='work-item-flex-item-right'>
-              {props.creativeName}
+            style={{ backgroundImage: `url(${props.filePath})` }}>
+            <div className='tags-container'>
+              {props.tags.map(tag => {
+                return <div className='tag'>{tag}</div>;
+              })}
             </div>
           </div>
         </Link>
       </div>
+
+      <Link
+        className='work-item-link'
+        to={{
+          pathname: '/workDetail',
+          state: {
+            userName: props.userName,
+            avatarImage: props.avatarImage,
+            token: props.token,
+            creative: props.creative,
+            creativeName: props.creativeName,
+            creativeAvatar: props.creativeAvatar,
+            categories: props.categories,
+            workId: props.workId
+          }
+        }}>
+        <div className='work-item-title-container'>
+          <img
+            className='showcase-category-icon'
+            src={`./images/${categoryIcon}-deselect.png`}
+            alt='Category icon'
+          />
+          <div className='work-item-title'>{props.fileTitle}</div>
+        </div>
+      </Link>
     </div>
   );
 };
